602379f683dbbdbc7192a83f14edcf7a566f941a
anh tuan <nguyenanhtuan050302@gmail.com>
2024-04-29 00:33:33 +0700

    T_fix_models

--- pythonMediaSocial/my_social_media_site/my_social_media/admin.py
+++ pythonMediaSocial/my_social_media_site/my_social_media/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Post, Like, Comment, Membership, Survey, Question, Answer, PostType, LikeType
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('username', 'email', 'date_joined', 'is_staff')
+    search_fields = ('username', 'email')
+    filter_horizontal = ()
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name','email', 'date_of_birth', 'number_phone', 'avatar', 'cover_photo')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Membership', {'fields': ('membership',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'avatar', 'cover_photo'),
+        }),
+    )
+
+# Đăng ký CustomUserAdmin với trang admin của Django
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(Membership)
+admin.site.register(Survey)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(PostType)
+admin.site.register(LikeType)
--- pythonMediaSocial/my_social_media_site/my_social_media_site/settings.py
+++ pythonMediaSocial/my_social_media_site/my_social_media_site/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_social_media.apps.MySocialMediaConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'my_social_media_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'social_media_network',
+        'USER': 'root',
+        'PASSWORD': '12345678',
+        'HOST': ''
     }
 }
 
@@ -121,3 +125,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'my_social_media.User'
+
+# cloudinary
+import cloudinary
+
+cloudinary.config(
+    cloud_name="dcezhzozn",
+    api_key="342611522918498",
+    api_secret="27wSws5sGd4sMAkutPAT9YH5EgA"
+)
