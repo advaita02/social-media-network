8125709f3963f8314b9b12dca34cf3213858c75f
advaita02 <lbnam1609@gmail.com>
2024-05-08 14:50:47 +0700

    abc

--- pythonMediaSocial/my_social_media_site/my_social_media/views.py
+++ pythonMediaSocial/my_social_media_site/my_social_media/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions, status, generics, parsers
 from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
-from .models import LikeType, Post, Comment, Like, User
-from .serializers import LikeTypeSerializer, PostSerializer, CommentSerializer, LikeSerializer, UserSerializer, PostDetailsSerializer
+from .models import LikeType, Post, Comment, Like, User, Membership, PostType
+from .serializers import LikeTypeSerializer, PostSerializer, CommentSerializer, LikeSerializer, UserSerializer, \
+    PostDetailsSerializer
 from .perms import OwnerPermission
 from django.shortcuts import get_object_or_404
 
@@ -37,7 +38,7 @@ class UserViewSet(viewsets.ViewSet, generics.ListAPIView,
 
 
 class PostViewSet(viewsets.ViewSet, generics.ListAPIView,
-                  generics.RetrieveAPIView):
+                  generics.RetrieveAPIView, generics.UpdateAPIView):
     queryset = Post.objects.filter(active=True).all()
     serializer_class = PostDetailsSerializer
     permission_classes = [permissions.AllowAny]
@@ -76,7 +77,7 @@ class PostViewSet(viewsets.ViewSet, generics.ListAPIView,
     @action(methods=['post'], url_path='like', detail=True)
     def add_like(self, request, pk):
         like, created = Like.objects.get_or_create(user=request.user,
-                                                      post=self.get_object())
+                                                   post=self.get_object())
         if not created:
             like.active = not like.active
             like.save()
@@ -108,3 +109,55 @@ class LikeViewSet(viewsets.ModelViewSet):
 class LikeTypeViewSet(viewsets.ModelViewSet):
     queryset = LikeType.objects.all()
     serializer_class = LikeTypeSerializer
+
+
+class LikeTypeListAPIView(generics.ListCreateAPIView):
+    queryset = LikeType.objects.all()
+    serializer_class = LikeTypeSerializer
+
+
+class LikeTypeDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = LikeType.objects.all()
+    serializer_class = LikeTypeSerializer
+
+
+# membership1 = Membership.objects.create(group_name='default')
+# membership2 = Membership.objects.create(group_name='Cuu sinh vien 2020')
+#
+# post_type1 = PostType.objects.create(name_type='Bao cao')
+# post_type2 = PostType.objects.create(name_type='default')
+#
+# like_type1 = LikeType.objects.create(name_type='like')
+# like_type2 = LikeType.objects.create(name_type='love')
+# #
+# user1 = User.objects.create(username='userNam', email='userNam@gmail.com')
+# user1.set_password('123456')
+# user1.save()
+#
+# user2 = User.objects.create(username='userTuan', email='userTuan@gmail.com')
+# user2.set_password('123456')
+# user2.save()
+#
+# post1 = Post.objects.create(
+#     title='Python da loi thoi',
+#     content='Vi no qua don gian va phuc tap',
+#     type_of_post=post_type1,
+#     created_by=user1
+# )
+#
+# post2 = Post.objects.create(
+#     title='Python la ngon ngu tuong lai the gioi',
+#     content='Vi la ngon ngu tuong lai nen can tao clb bat tran.',
+#     type_of_post=post_type2,
+#     created_by=user2
+# )
+#
+# post1.membership.add(membership1)
+# post1.membership.add(membership2)
+# post2.membership.add(membership1)
+#
+# like = Like.objects.create(
+#     user=user1,
+#     post=post1,
+#     type_of_like=like_type1
+# )
